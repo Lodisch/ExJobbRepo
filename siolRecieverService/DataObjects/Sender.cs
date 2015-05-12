aec787ad54daaa4d8e69955a67cef38aead04bdb
SimonO <simon_lod@hotmail.com>
2015-05-12 10:28:37 +0200

    All of it

---
+++ siolRecieverService/DataObjects/Sender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace siolRecieverService.DataObjects
+{
+    public class Sender
+    {
+        public Announcement Announcement { get; set; }
+        public Receiver Receiver { get; set; }
+    }
+}
